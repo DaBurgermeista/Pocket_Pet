3b5f64e6cb1c7a9f33dc5e02afbeef39d2dcaa4d
DaBurgermeista <63371712+DaBurgermeista@users.noreply.github.com>
2025-05-28 18:13:18 +0000

    Init Push

---
+++ scripts/main.js
@@ -0,0 +1,98 @@
+
+// Create our pet object
+const pet = {
+    hunger: 100,
+    energy: 100,
+    mood: 100,
+};
+
+// On document load...
+document.addEventListener("DOMContentLoaded", () => {
+    renderStats();
+
+    setInterval(statDecay, 1000);
+});
+
+function statDecay(){
+    pet.hunger -= 1;
+    if (pet.hunger < 0) {
+        pet.hunger = 0;
+    }
+
+    pet.energy -= Math.random() < 0.33 ? 1 : 0;
+    if (pet.energy < 0){
+        pet.energy = 0;
+    }
+
+    if (Math.random() < 0.5){
+        pet.mood -= 1;
+    }
+    // If hunger or energy are low, lose mood faster.
+    if (pet.hunger < 25 || pet.energy < 25){
+        pet.mood -= 1;
+    }
+    if (pet.mood < 0) {
+        pet.mood = 0;
+    }
+
+    renderStats();
+}
+
+function renderStats() {
+    // Assign HTML elements to variables
+    const hungerStat = document.getElementById('hunger-value');
+    const energyStat = document.getElementById('energy-value');
+    const moodStat = document.getElementById('mood-value');
+
+    //TODO: #1 Have values go red when they get low.
+    hungerStat.textContent = pet.hunger;
+    energyStat.textContent = pet.energy;
+    moodStat.textContent = pet.mood;
+}
+
+// Feed Button Listener
+document.getElementById('feed-btn').addEventListener('click', () => {
+    console.log("Clicked Feed");
+    pet.hunger += 10;
+    if (pet.hunger > 100){
+        pet.hunger = 100;
+    }
+    renderStats();
+});
+
+// Sleep Button Listener
+document.getElementById('sleep-btn').addEventListener('click', () => {
+    pet.energy += 15;
+    if (pet.energy > 100) {
+        pet.energy = 100;
+    }
+
+    pet.hunger -= 5;
+    if (pet.hunger < 0) {
+        pet.hunger = 0;
+    }
+    renderStats();
+});
+
+// Play Button Listener
+document.getElementById('play-btn').addEventListener('click', () => {
+    pet.mood += 10;
+    if (pet.mood > 100) {
+        pet.mood = 100;
+    }
+
+    pet.energy -= 10;
+    if (pet.energy < 0) {
+        pet.energy = 0;
+    }
+    renderStats();
+});
+
+// Clean Button Listener
+document.getElementById('clean-btn').addEventListener('click', () => {
+    pet.mood += 5;
+    if (pet.mood > 100) {
+        pet.mood = 100;
+    }
+    renderStats();
+});
